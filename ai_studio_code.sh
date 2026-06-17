7ddff6c479516e8aebceeb5aa3cc044b217b0e57
csatsurvey <haliunaa.dolgorjav@gmail.com>
2026-06-17 17:18:39 +0800

    Add files via upload

---
+++ ai_studio_code.sh
@@ -0,0 +1,6 @@
+git init
+git remote add origin https://github.com/csatsurvey/Menu_CSAT.git
+git add .
+git commit -m "feat: initial commit of restaurant menu and csat system"
+git branch -M main
+git push -u origin main
